2b4bdc8c713ca531d6316ba1a05d7c04996ca417
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-09-10 11:34:38 +0300

    better architectures for v0.03

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/RazanakotoMandresy/Tui-English-dico/pkg/getWord"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// TODO refactoring the code
+// TODO adding the way to add word
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(getWord.InialGet())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
